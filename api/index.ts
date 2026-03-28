039b00a006316037574f478aadb081207823ca41
clebinho1408 <135294265+clebinho1408@users.noreply.github.com>
2026-03-28 00:34:56 -0300

    v254

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,10 @@
 import { createServer } from '../lib/server.js';
 
+let app: any;
+
 export default async (req: any, res: any) => {
-  const app = await createServer();
+  if (!app) {
+    app = await createServer();
+  }
   return app(req, res);
 };
